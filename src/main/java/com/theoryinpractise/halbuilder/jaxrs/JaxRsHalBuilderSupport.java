a35e0ce50e3333812e5b20ddd618a21a58770c8d
Mikel Corcuera <mik.corcuera@gmail.com>
2014-06-14 12:43:45 +0200

    License Changes

--- src/main/java/com/theoryinpractise/halbuilder/jaxrs/JaxRsHalBuilderSupport.java
+++ src/main/java/com/theoryinpractise/halbuilder/jaxrs/JaxRsHalBuilderSupport.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright (C) 2014 Mikel Corcuera <mik.corcuera@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.theoryinpractise.halbuilder.jaxrs;
 
 import com.holacampus.api.exceptions.HTTPErrorException;
-import com.holacampus.api.resources.UsersResource;
 import com.holacampus.api.utils.HALBuilderUtils;
 import com.theoryinpractise.halbuilder.api.ReadableRepresentation;
 import com.theoryinpractise.halbuilder.api.RepresentationException;
@@ -12,7 +28,6 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import javax.ws.rs.BadRequestException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
